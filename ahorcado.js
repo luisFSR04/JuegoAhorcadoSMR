8d4d4595f0fa6266cfc7a721ac0b8738dd4ebfe7
Luis Fernando Salvatierra Romero <lsalvatierraromero@gmail.com>
2023-11-08 20:24:25 +0100

    Empiezo el js desde cero

--- ahorcado.js
+++ ahorcado.js
@@ -1,37 +1,18 @@
-String.prototype.replaceAt=function(index, character) {return this.substring(0, index) + character + this.substring(index+character.length); }
+String.prototype.replaceAt=function(index, character) { return this.substring(0, index) + character + this.substring(index+character.length); } 
 
 
-//Uso: palabraConGuiones = palabraConGuiones.replaceAt(i*2, letra);
+//Uso: palabraConGuiones = palabraConGuiones.replaceAt(*2, letra);
 
-const palabras = ['casa','perro','gato','elefante'];
+const palabras = ['pelota', 'Valentín', 'mamaguevo', 'doble elefante telepata de guerra'];
 
 const palabra = palabras[Math.floor(Math.random()*palabras.length)];
 const palabraConGuiones = palabra.replace(/./g, "_ ");
-let contadorFallos = 0; 
-document.querySelector('#output').innerHTML = palabraConGuiones;
+
 document.querySelector('#calcular').addEventListener('click', () =>{
 	const letra = document.querySelector('#letra').value;
-	let haFallado = true;	
+
 	for(const i in palabra){
-		if(letra == palabra[i]) {
-			palabraConGuiones = palabraConGuiones.replaceAt (i*2, letra);
-			haFallado = false; 
-		}
+		if(letra == palabra[i])
 	}
-
-	if(haFallado){
-		contadorFallos++;
-		document.querySelector('#ahorcado').style.backgroundPosition = -(307*contadorFallos) + 'px 0';
-		if(contadorFallos == 5){
-			alert("Por tu incompetencia ha muerto")
-		}
-	}else{
-		if(palabraConGuiones.indexOf('_') <0){
-			document.querySelector('#ganador').style.display = 'flex';
-		}
-	}
-
-	document.querySelector('#output').innerHTML = palabraConGuiones;
-	document.querySelector('letra').value = ''; 
-	document.querySelector('letra').focus();
-});
+	alert(letra)
+})

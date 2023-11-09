762e37c91ca5297101674a9002acf66d5c479ccd
Luis Fernando Salvatierra Romero <lsalvatierraromero@gmail.com>
2023-11-09 18:30:11 +0100

    .

--- ahorcado.js
+++ ahorcado.js
@@ -3,19 +3,36 @@ String.prototype.replaceAt=function(index, character) { return this.substring(0,
 
 //Uso: palabraConGuiones = palabraConGuiones.replaceAt(*2, letra);
 
-const palabras = ['pelota', 'Valentín', 'mamaguevo', 'doble elefante telepata de guerra'];
+const palabras = ['perro', 'gato', 'casa', 'elefante'];
 
 const palabra = palabras[Math.floor(Math.random()*palabras.length)];
-let const palabraConGuiones = palabra.replace(/./g, "_ ");
-
+let palabraConGuiones = palabra.replace(/./g, "_ ");
+let contadorFallos = 0
 document.querySelector('output').innerHTML = palabraConGuiones
 document.querySelector('#calcular').addEventListener('click', () =>{
 	const letra = document.querySelector('#letra').value;
-	alert(palabra)
-
+	lethaFallado = true;}
+)
 	for(const i in palabra){
 		if(letra == palabra[i])
 		palabraConGuiones = palabraConGuiones.replace(i*2, letra);
+		haFallado = false;
+	}
+
+	if(haFallado) {
+		contadorFallos++;
+		document.querySelector('#ahorcado').style.backgroundPosition = -(307*contadorFallos) + 'px'
+		if(contadorFallos == 4){
+			alert("perdiste el juego")
 		}
+	}else{
+		if(palabraConGuiones.indexOf('_') <0){
+			document.querySelector('#ganador').style.display ='flex'
+	}
+
+
 		document.querySelector('output').innerHTML = palabraConGuiones
-})
+
+	document.querySelector('#letra').value = '';
+	document.querySelector('#letra').focus();
+};
